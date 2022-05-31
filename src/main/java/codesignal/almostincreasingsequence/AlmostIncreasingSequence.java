089acc163487e45fe0568959c5250d38bd0d61b2
Uzytkownik <uzytkownik@kursGIT.pl>
2022-05-31 19:00:10 +0200

    AlmostIncreasingSequence_solution2

--- src/main/java/codesignal/almostincreasingsequence/AlmostIncreasingSequence.java
+++ src/main/java/codesignal/almostincreasingsequence/AlmostIncreasingSequence.java
@@ -9,55 +9,14 @@ public class AlmostIncreasingSequence {
     }
 
     public static boolean almostIncreasingSequence(int[] sequence) {
-
-        int offenderPosition = 0;
-        int[] arrCopy = Arrays.copyOf(sequence, sequence.length);
-        boolean ordered = true;
-
-
-//trying to neatly rewrite this for loop
-        for (int i = 0; i < sequence.length; i++) {
-            if (i < sequence.length - 1) {
-                for (int j = i + 1; j < sequence.length; j++) {
-                    if (!(sequence[i] < sequence[j])) {
-                        ordered = false;
-                        offenderPosition = i;
-                    }
-                }
-            }
-            if (i == sequence.length - 1) {
-                if (!(sequence[i] > sequence[i - 1])) {
-                    ordered = false;
-                    offenderPosition = i;
-                }
-            }
-
+        int count1 = 0 , count2 = 0;
+        for(int i = 0 ; i < sequence.length-1 ; i++){
+            if(sequence[i] >= sequence[i+1]) count1++;
         }
 
-
-        if (ordered == false) {
-            //remove offender
-            int currentSize = arrCopy.length;
-            for (int i = offenderPosition + 1; i < currentSize; i++) {
-                arrCopy[i - 1] = arrCopy[i];
-            }
-            currentSize--;
-
-            //reassign array
-            arrCopy = Arrays.copyOf(arrCopy, currentSize);
-
-            boolean lastChance = true;
-
-            for (int i = 0; i < currentSize - 1; i++) {
-                for (int j = i + 1; j < currentSize; j++) {
-                    if (!(arrCopy[i] < arrCopy[j])) {
-                        lastChance = false;
-                    }
-                }
-            }
-            return lastChance;
-        } else{
-            return true;
+        for(int i = 0 ; i < sequence.length-2 ; i++){
+            if(sequence[i] >= sequence[i+2]) count2++;
         }
+        return (count1 <=1) && (count2 <= 1);
     }
 }
